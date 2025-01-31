411e3df10f72c7c0c71ea5afe2c2cd703b518b10
Jelle Wilbrink <49995469+jellewilbrink@users.noreply.github.com>
2025-01-31 20:49:36 +0100

    Update example secrets for BLE

--- src/example_secrets.h
+++ src/example_secrets.h
@@ -1,14 +1,14 @@
-#define SSID "INSERT WIFI NAME"
-#define WIFI_PASSWORD "INSERT WIFI PASSWORD"
-
 // Number of IP addresses to ping
-#define NUM_ADDRESSES 2
+#define NUM_UUIDs 2
 
-// List of IP adresses to ping. Length must be NUM_ADDRESSES
-#define IP_ADDRESSES {"192.168.2.1", "192.168.2.2"}
+// List of IP adresses to ping. Length must be NUM_UUIDs
+#define UUID_LIST {"ABCD", "EF01"}
 
-// List of GPIO pins for the LEDs. Length must be NUM_ADDRESSES
+// List of GPIO pins for the LEDs. Length must be NUM_UUIDs
 #define LED_PINS {13, 14}
 
-// Time between consecutive pings in ms
-#define PING_DELAY 10000
+// Duration of a single scan in ms
+#define SCAN_TIME 10000
+
+// Time between consecutive scans in ms
+#define SCAN_INTERVAL 60000
