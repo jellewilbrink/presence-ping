0dc1d19e198ccb90c5b3359b2eb374cfde433781
Jelle Wilbrink <49995469+jellewilbrink@users.noreply.github.com>
2025-02-08 13:12:15 +0100

    Add scan_and_print() for debugging

--- src/ble_scanner.cpp
+++ src/ble_scanner.cpp
@@ -1,4 +1,5 @@
 #include "ble_scanner.hpp"
+#include <Arduino.h>
 
 BleScanner::BleScanner()
 {
@@ -17,6 +18,35 @@ bool BleScanner::is_device_found(uint64_t device_address)
     NimBLEAddress nimble_address(device_address, ble_address_type);
 
     return scan_results.getDevice(nimble_address) != nullptr ? true : false;
+}
+
+void BleScanner::scan_and_print(uint32_t duration, unsigned long baud_rate)
+{
+    Serial.begin(baud_rate);
+
+    Serial.print("Scanning for ");
+    Serial.print(duration);
+    Serial.println(" ms.");
+    scan(duration); // Blocking scan
+
+    int num_found = scan_results.getCount();
+    Serial.print("Found ");
+    Serial.print(num_found);
+    Serial.println(" devices:");
+
+    for (int i = 0; i < num_found; i++)
+    {
+        const NimBLEAdvertisedDevice *device = scan_results.getDevice(i);
+        std::string dev_string = device->toString();
+        Serial.println(dev_string.c_str());
+
+        Serial.print("RSSI: ");
+        Serial.print(device->getRSSI());
+        Serial.println("");
+        Serial.println("");
+    }
+    Serial.println("");
+    Serial.println("");
 }
 
 void BleScanner::init()
--- src/ble_scanner.hpp
+++ src/ble_scanner.hpp
@@ -11,6 +11,14 @@ public:
     void scan(uint32_t duration);
     bool is_device_found(uint64_t device_address);
 
+    /**
+     * Scan for BLE devices and print the results to serial console for debugging purposes.
+     *
+     * \note This function makes no assumptions about Serial.begin(baud_rate) having been
+     * called before, so it will call it just to be sure.
+     */
+    void scan_and_print(uint32_t duration, unsigned long baud_rate);
+
 private:
     void init();
 
